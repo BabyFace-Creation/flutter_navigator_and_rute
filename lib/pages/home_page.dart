3be859737b78a0fe5d110da58846c238157cfc5f
BabyFace-Creation <andriy.atha@gmail.com>
2021-03-22 16:51:18 +0700

    Changing Navigator.push to Navogator.pushNamed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,62 +10,62 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Rute and Navigate"),
-          centerTitle: true,
-        ),
-        body: Container(
-          margin: EdgeInsets.all(8),
-          child: ListView.builder(
-              padding: EdgeInsets.all(8),
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                final item = items[index];
-                return Card(
-                    child: Container(
-                  margin: EdgeInsets.all(8),
-                  child: InkWell(
-                    onTap: () {
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return new AlertDialog(
-                              title: new Text("Detail " + item.name),
-                              actions: <Widget>[
-                                new FlatButton(
-                                  onPressed: () => Navigator.of(context).pop(),
-                                  child: Text("Back"),
-                                ),
-                                new FlatButton(
-                                  onPressed: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                ItemPage(tempItem: item)));
-                                  },
-                                  child: new Text("Detail"),
-                                ),
-                              ],
-                            );
-                          });
-                    },
-                    child: Row(
-                      children: [
-                        Expanded(child: Text(item.name)),
-                        Expanded(
-                            child: Text(
-                          item.price.toString(),
-                          textAlign: TextAlign.end,
-                        )),
-                      ],
-                    ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Rute and Navigate"),
+        centerTitle: true,
+      ),
+      body: Container(
+        margin: EdgeInsets.all(8),
+        child: ListView.builder(
+            padding: EdgeInsets.all(8),
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final item = items[index];
+              return Card(
+                  child: Container(
+                margin: EdgeInsets.all(8),
+                child: InkWell(
+                  onTap: () {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return new AlertDialog(
+                            title: new Text("Detail " + item.name),
+                            actions: <Widget>[
+                              new FlatButton(
+                                onPressed: () => Navigator.of(context).pop(),
+                                child: Text("Back"),
+                              ),
+                              new FlatButton(
+                                onPressed: () {
+                                  Navigator.pushNamed(context, '/item',
+                                      arguments: item);
+                                  // Navigator.push(
+                                  //     context,
+                                  //     MaterialPageRoute(
+                                  //         builder: (context) =>
+                                  //             ItemPage(tempItem: item)));
+                                },
+                                child: new Text("Detail"),
+                              ),
+                            ],
+                          );
+                        });
+                  },
+                  child: Row(
+                    children: [
+                      Expanded(child: Text(item.name)),
+                      Expanded(
+                          child: Text(
+                        item.price.toString(),
+                        textAlign: TextAlign.end,
+                      )),
+                    ],
                   ),
-                ));
-              }),
-        ),
+                ),
+              ));
+            }),
       ),
     );
   }
